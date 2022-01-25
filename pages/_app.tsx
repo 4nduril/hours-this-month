c1da34178f831354e90f099a25be61eddcad8c83
Tobias Barth <mail@tobias-barth.net>
2022-01-25 14:09:48 +0100

    feat: add next-seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { DefaultSeo } from 'next-seo'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo
+        title="As time goes by"
+        description="Find out how much time went by between now and then. Years, months, weeks, days, hours, minutes, seconds – they all will be lost in time like tears in rain."
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
