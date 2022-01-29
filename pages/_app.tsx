a89aab9480b4a77c3d24626f8f1318dd6a2d78d1
Tobias Barth <mail@tobias-barth.net>
2022-01-29 11:44:58 +0100

    feat: add plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { DefaultSeo } from 'next-seo'
+import PlausibleProvider from 'next-plausible'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,7 +10,12 @@ function MyApp({ Component, pageProps }: AppProps) {
         title="As time goes by"
         description="Find out how much time went by between now and then. Years, months, weeks, days, hours, minutes, seconds – they all will be lost in time like tears in rain."
       />
-      <Component {...pageProps} />
+      <PlausibleProvider
+        domain="as-time-goes-by.dev"
+        enabled={process.env.VERCEL_ENV === 'production'}
+      >
+        <Component {...pageProps} />
+      </PlausibleProvider>
     </>
   )
 }
